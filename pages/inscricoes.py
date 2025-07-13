a308e0a1e19b564bb45d098cf6c7689a2addf532
Marco Junior <84521185+marcojr93@users.noreply.github.com>
2025-07-13 13:46:24 -0400

    Update inscricoes.py

--- pages/inscricoes.py
+++ pages/inscricoes.py
@@ -79,8 +79,8 @@ with st.form("form_inscricao"):
 
                 # Envia e-mail para o organizador
                 try:
-                    remetente = os.environ.get("marcojunior.em@gmail.com")
-                    senha = os.environ.get("ykru noqj asdf ysjv")
+                    remetente = "marcojunior.em@gmail.com"
+                    senha = "ykru noqj asdf ysjv"
                     destinatario = "bianca.s.cordeiro@gmail.com"
                     assunto = f"Nova inscrição: {dados_inscricao['equipe']} ({dados_inscricao['data']})"
                     corpo = f"""
